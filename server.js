85fd06e58ed2b638905e33a0acdf7324b016f712
Gabe Perry <gperry22@gmaili.com>
2018-04-20 22:25:14 -0400

    Folder Structure First Push

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require("./routes");
+const db = require("./models");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactMongoScraper";
+
+mongoose.Promise = global.Promise;
+
+
+mongoose.connect(MONGODB_URI);
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
+
+
+
